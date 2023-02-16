3670a62bb7d475da2517c15bbc4fca4fce839a9d
bbt420 <olaleyetosinthompson@gmail.com>
2023-02-16 11:29:39 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
  * main - print out exactly the quote
@@ -7,6 +8,6 @@
 
 int main(void)
 {
-	write(2,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n",59);
-	return (1);
+fputs("and that piece of art is useful" - Dora Korpar, 2015-10-19\n", stderr);
+return (1);
 }
