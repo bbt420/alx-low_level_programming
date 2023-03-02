4b964c14870f4852772dd230e04bd641f8ba5e1b
bbt420 <olaleyetosinthompson@gmail.com>
2023-03-02 04:24:02 -0800

    task eight

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -5,21 +5,29 @@
  * rot13 - encoder rot13
  * @s: pointer to string params
  *
- * Return: *s
+ * Return: pointer to encoded string
  */
 
 char *rot13(char *s)
 {
 	int i;
 	int j;
-	char data1[] = "ABCDEFGHIJKLMNOPRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char data1[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+			'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+			'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+			'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+			'Z'};
+	char datarot[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+			'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+			'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+			'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+			'M'};
 
 	for (i = 0 ; s[i] != '\0' ; i++)
 	{
-		for ( j = 0 ; j < 52 ; j++)
+		for (j = 0 ; j < 53 ; j++)
 		{
-			if (s[i] == data1[j])
+			if (data1[j] == s[i])
 			{
 				s[i] = datarot[j];
 				break;
