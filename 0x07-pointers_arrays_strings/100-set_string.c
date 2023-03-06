71a1fa8365da5375416f28b1bf6b59c9a64a9125
bbt420 <olaleyetosinthompson@gmail.com>
2023-03-06 07:42:29 -0800

    task eight

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - entry point
+ * @s: input parameter
+ * @to: input parameter
+ * Return: always 0
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
