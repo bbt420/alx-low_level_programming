e8f7944604e4daeac23b460b43acc616b7006c85
bbt420 <olaleyetosinthompson@gmail.com>
2023-03-22 08:03:23 -0700

    task three

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -23,7 +23,7 @@ int op_add(int a, int b)
  */
 int op_sub(int a, int b)
 {
-        return (a - b);
+	return (a - b);
 }
 
 /**
@@ -35,7 +35,7 @@ int op_sub(int a, int b)
  */
 int op_mul(int a, int b)
 {
-        return (a * b);
+	return (a * b);
 }
 
 /**
@@ -47,7 +47,7 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
-        if (b == 0)
+	if (b == 0)
 	{
 		printf("Error\n");
 		exit(100);
@@ -64,10 +64,10 @@ int op_div(int a, int b)
  */
 int op_mod(int a, int b)
 {
-        if (b == 0)
-        {       
-                printf("Error\n");
-                exit(100);
-        }
-        return (a % b);
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
 }
